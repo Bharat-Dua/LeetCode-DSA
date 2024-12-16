892f3ace1482f44d0b12d48c7a5e09b64b2941dc
Bharat-Dua <bharatdua13@gmail.com>
2024-12-16 14:10:46 +0530

    solved

--- Binary-Tree_or_Binary-Search-Tree/102. Binary Tree Level Order Traversal.js
+++ Binary-Tree_or_Binary-Search-Tree/102. Binary Tree Level Order Traversal.js
@@ -1,3 +1,4 @@
+//* TC,SC- O(n)
 class Node {
   constructor(value) {
     this.value = value;
@@ -13,13 +14,43 @@ class BinaryTree {
   insert(array) {
     if (!array.length) return;
     let i = 0;
+    // if root is null
     if (!this.root) {
       if (array[0] === null) return;
       else {
         let node = new Node(array[0]);
         this.root = node;
         i++;
-        if (i === array.length) return;
+        if (i === array.length) return this;
+      }
+    }
+    // insert remaining elements in queue
+    let queue = [this.root];
+    while (queue.length) {
+      let current = queue.shift();
+      // left
+      if (!current.left) {
+        if (array[i] !== null) {
+          let node = new Node(array[i]);
+          current.left = node;
+        }
+        i++;
+        if (i === array.length) return this;
+      }
+      if (current.left) {
+        queue.push(current.left);
+      }
+      // Right
+      if (!current.right) {
+        if (array[i] !== null) {
+          let node = new Node(array[i]);
+          current.right = node;
+        }
+        i++;
+        if (i === array.length) return this;
+      }
+      if (current.right) {
+        queue.push(current.right);
       }
     }
   }
@@ -28,7 +59,20 @@ class BinaryTree {
 const levelOrderTraversal = function (root) {
   if (!root) return [];
   const output = [];
-  //write code here
+  const queue = [root];
+  while (queue.length) {
+    let count = 0;
+    let currLevel = [];
+    let length = queue.length;
+    while (count < length) {
+      let node = queue.shift();
+      currLevel.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+      count++;
+    }
+    output.push(currLevel);
+  }
   return output;
 };
 
