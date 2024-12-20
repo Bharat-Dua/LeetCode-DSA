6f088be56694f4ce809c3c742a70bd018d7a7a2e
Bharat-Dua <bharatdua13@gmail.com>
2024-12-20 22:49:18 +0530

    solved build function heap

---
+++ Heaps/Construct-max-binary-heap.js
@@ -0,0 +1,47 @@
+class buildBinaryHeap {
+  constructor() {
+    this.heap = [];
+  }
+  buildHeap(array) {
+    let length = array.length;
+    let lastParent = Math.floor(length / 2 - 1);
+    for (let i = lastParent; i >= 0; i--) {
+      this.bubbleDown(array, i);
+    } // heapify the array
+    this.heap = array;
+    return this;
+  }
+  bubbleDown(array, idx) {
+    let current = array[idx];
+    let length = array.length;
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let largest = null;
+      if (leftChildIdx < length) {
+        leftChild = array[leftChildIdx];
+        if (leftChild > current) {
+          largest = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = array[rightChildIdx];
+        if (
+          (largest === null && rightChild > current) ||
+          (largest !== null && rightChild > leftChild)
+        ) {
+          largest = rightChildIdx;
+        }
+      }
+      if (largest === null) break;
+      // else swap
+      array[idx] = array[largest];
+      array[largest] = current;
+      idx = largest;
+    }
+  }
+}
+
+let binaryHeap = new buildBinaryHeap();
+binaryHeap.buildHeap([10, 20, 5, 6, 12, 3, 8, 1, 9, 2]);
